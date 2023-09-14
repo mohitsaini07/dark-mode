5b7ef2df2b3d2a5ab58e9fce946d58f13321a549
Mohit Saini <mohitbhatwara2@gmail.com>
2023-09-14 10:57:25 +0530

    fixed switch to light-mode

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-const moonImage = "/dark theme icon/moon.png";
-const sunImage = "/dark theme icon/sun.png";
+const moonImage = "./assests/moon.png";
+const sunImage = "./assests/sun.png";
 
 // Reference of required elements
 const icon = document.querySelector("#icon");
 const darkModeToggle = document.querySelector("#darkModeToggle");
 
-
 // Function to toggle dark mode:
 function toggleDarkMode() {
   const body = document.getElementById("body");
-  body.classList.toggle("dark-mode");
+  const isDarkMode = body.classList.toggle("dark-mode");
 
-  if (body.classList.contains('dark-mode')) {
-    icon.setAttribute("src", sunImage)
+  if (isDarkMode) {
+    icon.setAttribute("src", sunImage);
   } else {
-    icon.setAttribute("src", moonImage)
+    icon.setAttribute("src", moonImage);
   }
-
 }
 
 darkModeToggle.addEventListener("change", toggleDarkMode);
-
-
